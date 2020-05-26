5601c61773adb7ca932ae71eb6799e7176eac0c6
a885566885566 <a885566885566@gmail.com>
2020-05-26 21:10:16 +0800

    font-size use rem, new name_list format

--- public/main.js
+++ public/main.js
@@ -12,8 +12,20 @@ var addCols = function (data){
         }*/
         var myCol = $('<div class="list_box"></div>');
         console.log(group);
-        $(block_id).find
-        var myPanel = $('<div class="card-block" id="'+block_id+'"><div class="card-header card_title"><span id="Professor">'+group["Teacher"]+'</span></div><div class="card-body card_content"><div id="Students" class="bachelor">'+group["Student"]["a"]+' </div><div id="Masters" class="master">'+group["Student"]["b"]+' </div></div></div>');
+        //$(block_id).find
+        var info = $("<div class='card-body card_content'></div>")
+        var skeys = Object.keys(group.Student)
+        skeys.forEach((k)=>{
+            if( group.Student[k] != "")
+                info.append($("<div id='Students' class='bachelor'></div>")
+                    .text(k +": "+ group.Student[k]))
+        })
+        var myPanel = $("<div class='card-block'></div>").attr("id", block_id)
+            .append($("<div class='card-header card_title'>")
+                .append($("<span id='Professor'></span>").text(group["Teacher"])
+                .append(info))
+            )
+        //var myPanel = $('<div class="card-block" id="'+block_id+'"><div class="card-header card_title"><span id="Professor">'+group["Teacher"]+'</span></div><div class="card-body card_content"><div id="Students" class="bachelor">'+group["Student"]["a"]+' </div><div id="Masters" class="master">'+group["Student"]["b"]+' </div></div></div>');
         myPanel.appendTo(myCol);
         myCol.appendTo('#panels');
     }
